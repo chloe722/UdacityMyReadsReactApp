e17631a81dadcdc5ba32344e8aaf54ae7fc74a6c
Chloe Hsu <vivi831018@hotmail.com>
2018-08-01 21:46:00 +0800

    Join multiple authors using comma

--- src/Book.js
+++ src/Book.js
@@ -34,7 +34,7 @@ class Book extends React.PureComponent {
           </div>
         </div>
         <div className="book-title">{this.props.title}</div>
-        <div className="book-authors">{this.props.authors}</div>
+        <div className="book-authors">{this.props.authors ? this.props.authors.join(', ') : ''}</div>
       </div>
       </li>
       )
